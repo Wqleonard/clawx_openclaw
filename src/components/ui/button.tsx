8cf6e7eef544bf5ebc74fc93ef171f6dc1cf5a1c
Johnny <jiangwenjie.johnny@shengqugames.com>
2026-03-18 17:10:33 +0800

    feat: 新增将状态管理移动到TitleBar

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -20,7 +20,7 @@ const buttonVariants = cva(
           'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
         secondary:
           'bg-secondary text-secondary-foreground hover:bg-secondary/80',
-        ghost: 'hover:bg-accent hover:text-accent-foreground',
+        ghost: 'hover:bg-black/5 dark:hover:bg-white/10',
         link: 'text-primary underline-offset-4 hover:underline',
       },
       size: {
