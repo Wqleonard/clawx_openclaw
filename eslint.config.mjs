71d5ebe95fbb511936fcd255a5c62295e0bd240a
Johnny <jiangwenjie.johnny@shengqugames.com>
2026-03-19 18:31:31 +0800

    fix: 修复显示了历史settings 按钮

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -36,7 +36,7 @@ export default [
         'warn',
         { allowConstantExport: true },
       ],
-      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_', destructuredArrayIgnorePattern: '^_' }],
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_', destructuredArrayIgnorePattern: '^_' }],
       '@typescript-eslint/no-explicit-any': 'warn',
     },
   },
